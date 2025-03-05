9a54ff963c64389f5c235033960be8ea30aee637
wallnetMac <wallnetmac1@gmail.com>
2025-03-05 18:07:57 +0100

    Aggiornata ticket list

--- src/app/weco/tickets/ticket-list/ticket-list.component.ts
+++ src/app/weco/tickets/ticket-list/ticket-list.component.ts
@@ -4,6 +4,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { TranslateModule } from '@ngx-translate/core';
 import { InViewportDirective } from '../../../directives/in-viewport.directive';
 import { TicketTableComponent } from "./components/ticket-table/ticket-table.component";
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ticket-list',
@@ -12,12 +13,22 @@ import { TicketTableComponent } from "./components/ticket-table/ticket-table.com
     CommonModule,
     MatExpansionModule,
     TranslateModule,
-    InViewportDirective,
-    TicketTableComponent
+    TicketTableComponent,
+    ReactiveFormsModule
 ],
   templateUrl: './ticket-list.component.html',
   styleUrl: './ticket-list.component.scss'
 })
 export class TicketListComponent {
+
+  submitted: boolean = false;
+  statusList: {id: number, name: string, color: string}[] = [];
+  //lastFilterSearch: {};
+  filterForm = new FormGroup({
+    status: new FormControl<number | null>(null),
+    system: new FormControl<string | null>(null),
+    technician: new FormControl<string | null>(null),
+    owner: new FormControl<string | null>(null),
+  })
 
 }
